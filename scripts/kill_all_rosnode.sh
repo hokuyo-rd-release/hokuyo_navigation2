c38b14a9d5149bd51b9d91e241ac19a2bcca5474
sanjayraturi <44591569+sanjayraturi@users.noreply.github.com>
2025-04-07 13:36:24 +0900

    Update kill_all_rosnode.sh

--- scripts/kill_all_rosnode.sh
+++ scripts/kill_all_rosnode.sh
@@ -1,4 +1,16 @@
 #!/bin/bash
 echo "Kill all ros nodes!"
-ps aux | grep ros | grep -v grep | awk '{ print "kill -9", $2 }' | sh
+echo "Closing rosbag terminal!"
+pkill -f rosbag
+sleep 1s
+echo "Closing launch terminal!"
+pkill -f wizurg_start
+sleep 1s
+echo "Closing roscore terminal!"
+pkill -f roscore
+sleep 1s
+echo "Closing ypspur terminal!"
+pkill -f ypspur
+sleep 1s
+#ps aux | grep ros | grep -v grep | awk '{ print "kill -9", $2 }' | sh
 exit 0
