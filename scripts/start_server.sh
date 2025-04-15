236c621a38b912547a5f50db55f1e513bd11bf83
s-takahashijp <shallow.e1621@gmail.com>
2025-04-15 22:23:50 +0900

    04/15 ウィンドウの最小化

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -4,9 +4,18 @@ source /opt/ros/noetic/setup.bash
 cd ~/catkin_ws
 source devel/setup.bash
 source ~/.bashrc
-roscd expo_wizurg 
+roscd expo_wizurg
 
-gnome-terminal --tab -- bash -c "python3 /home/takahashi/github/expo_software/takahashi/expo_gui/server.py ; bash"
-gnome-terminal --tab -- bash -c "roslaunch vizanti server.launch ; bash"
+gnome-terminal --tab -- bash -c "python3 /home/takahashi/github/expo_software/takahashi/expo_gui/server.py ; bash" &
+sleep 1
+gnome-terminal --tab -- bash -c "roslaunch vizanti server.launch ; bash" &
+sleep 3
+gnome-terminal --tab -- bash -c "roslaunch rosbridge_server rosbridge_websocket.launch ; bash" &
+sleep 1
+
 sleep 2
-gnome-terminal --tab -- bash -c "roslaunch rosbridge_server rosbridge_websocket.launch ; bash"
+
+# 全ての gnome-terminal ウィンドウの ID を取得して最小化
+for id in $(xdotool search --class "gnome-terminal"); do
+    xdotool windowminimize $id
+done
