bd11c8de712f4809dacce3fc73ed2c06fbec09e1
s-takahashijp <shallow.e1621@gmail.com>
2025-10-15 15:18:29 +0000

    10/15 docker demo path changed

--- scripts/hokuyo_slam.bash
+++ scripts/hokuyo_slam.bash
@@ -4,11 +4,11 @@
 # コンテナの起動時に -e DOCKER_ENV=1 を指定することで、Docker環境とみなすことができます。
 if [ -n "$DOCKER_ENV" ]; then
     source /opt/ros/humble/setup.bash
-    cd ${HOME}/colcon_ws
+    cd /home/colcon_ws
     source install/setup.bash
     source ~/.bashrc
-    ROS2_WS="${HOME}/colcon_ws"
-    HOKUYO_SLAM_WS="${HOME}/github/hokuyo_slam_ros2"
+    ROS2_WS="/home/colcon_ws"
+    HOKUYO_SLAM_WS="/home/github/hokuyo_slam_ros2"
     HOKUYO_NAV2_PKG_PATH=${ROS2_WS}/src/hokuyo_navigation2
 else
     source /opt/ros/humble/setup.bash
