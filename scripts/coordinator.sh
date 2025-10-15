385ee6859a2466d577d21a70fb49e822e9a6b4f8
s-takahashijp <shallow.e1621@gmail.com>
2025-10-15 08:34:32 +0000

    10/15 docker demo

--- scripts/coordinator.sh
+++ scripts/coordinator.sh
@@ -4,11 +4,11 @@
 # コンテナの起動時に -e DOCKER_ENV=1 を指定することで、Docker環境とみなすことができます。
 if [ -n "$DOCKER_ENV" ]; then
     source /opt/ros/humble/setup.bash
-    cd ${HOME}/colcon_ws
+    cd /home/colcon_ws
     source install/setup.bash
     source ~/.bashrc
-    ROS2_WS="${HOME}/colcon_ws"
-    HOKUYO_NAV2_PKG_PATH="${HOME}/colcon_ws/src/hokuyo_navigation2"
+    ROS2_WS="/home/colcon_ws"
+    HOKUYO_NAV2_PKG_PATH="/home/colcon_ws/src/hokuyo_navigation2"
 else
     source /opt/ros/humble/setup.bash
     # ワークスペースのパスもホストOSのものに合わせる
